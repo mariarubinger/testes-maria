b07fb8f36993b9383321e2859a1a5a54d8b7c106
Marcus Rocha <marcus.rocha@ymc.ch>
2020-12-22 11:31:49 +0000

    Review and suggestions

--- calculadora-da-maria/script.js
+++ calculadora-da-maria/script.js
@@ -1,4 +1,20 @@
 function calc(num){ 
+
+    // let screen = document.getElementById("screen").value;
+    // if(screen){
+    //     var qtd_char = document.getElementById("screen").value.length;
+    //     var ultimo_char = screen.substring(qtd_char-1, qtd_char);
+    //     console.log("Ultimo char:" + ultimo_char)
+        
+        
+    
+    // }
+    // if((ultimo_char == '+' || ultimo_char == '-') &&
+    //     (num == '+' || num == '-')) {
+    //     return ;    
+    // }
+
+    // Marcus - This is my humble suggestion
     document.getElementById("screen").value += num;
 }
             
@@ -11,3 +27,14 @@ function total(){
     document.getElementById("screen").value = eval(result);
     // função eval(string) de javascript
 }
+
+
+// function runScript(e) {
+//     //See notes about 'which' and 'key'
+//     if (e.keyCode == 13) {
+//         alert('You press enter key')
+//     }
+// }
+
+
+//Only suggestion to improve!
